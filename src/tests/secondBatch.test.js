1e263eda39ed35207d050e0af5c7958ccee505f4
Ivan Vasilev <vanokvas@gmail.com>
2023-12-01 23:35:50 +0400

    Add jest describe

--- src/tests/secondBatch.test.js
+++ src/tests/secondBatch.test.js
@@ -1,38 +1,40 @@
 const {sleep} = require('./utils')
 
-test('the shopping list has milk on it', () => {
-  sleep(10)
-  expect(['milk', 'eggs', 'bread']).toContain('milk');
-})
+describe('second batch', () => {
+  test('the shopping list has milk on it', () => {
+    sleep(10)
+    expect(['milk', 'eggs', 'bread']).toContain('milk');
+  })
 
-function compileAndroidCode() {
-  throw new Error('you are using the wrong JDK');
-}
+  function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK');
+  }
 
-test('compiling android goes as expected', () => {
-  sleep(10)
-  expect(() => compileAndroidCode()).toThrow();
-  expect(() => compileAndroidCode()).toThrow(Error);
-})
+  test('compiling android goes as expected', () => {
+    sleep(10)
+    expect(() => compileAndroidCode()).toThrow();
+    expect(() => compileAndroidCode()).toThrow(Error);
+  })
 
-async function fetchData() {
-  return 'peanut butter'
-}
+  async function fetchData() {
+    return 'peanut butter'
+  }
 
-test('the data is peanut butter', async () => {
-  sleep(10)
-  const data = await fetchData();
-  expect(data).toBe('peanut butter')
-})
+  test('the data is peanut butter', async () => {
+    sleep(10)
+    const data = await fetchData();
+    expect(data).toBe('peanut butter')
+  })
 
-test('the data is peanut butter', () => {
-  sleep(10)
-  return fetchData().then(data => {
-    expect(data).toBe('peanut butter');
-  });
-})
+  test('the data is present', () => {
+    sleep(10)
+    return fetchData().then(data => {
+      expect(data).toBeTruthy();
+    });
+  })
 
-test('truthy test', () => {
-  sleep(10)
-  expect('Jest').toBeTruthy();
+  test('truthy test', () => {
+    sleep(10)
+    expect('Jest').toBeTruthy();
+  })
 })
